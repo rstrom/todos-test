524a7785f91f9ef90bd80abdfefb6840bf02610e
Robbie Strom <robbie.strom@gmail.com>
2017-11-30 09:40:04 +0100

    Set up cypress w basic specs

---
+++ cypress/integration/todos_spec.js
@@ -0,0 +1,7 @@
+describe("todos app", () => {
+  it("should load a sign page", () => {});
+
+  it("should load todos after signing in", () => {});
+
+  it("should load an individual todo", () => {});
+});
